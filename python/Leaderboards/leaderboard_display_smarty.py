da8d26b8ba183bc16a6c3340bf753801e8edee5b
jtsimmons108 <jsimmons@psdr3.org>
2017-12-03 11:14:38 -0600

    fixed formatting

--- python/Leaderboards/leaderboard_display_smarty.py
+++ python/Leaderboards/leaderboard_display_smarty.py
@@ -2,12 +2,12 @@ import json, dateutil.parser
 
 data = json.load(open('./smarty_leaderboard.json'))
 out_file = open('../../smarty_leaderboard.txt', 'w')
-print('Leaderboard for:', data['event'],'\n')
+out_file.write('Leaderboard for:' + data['event'] + '\n')
 members = data['members']
+place = 1
 for member, stars, local_score in sorted([(name, int(members[name]['stars']), int(members[name]['local_score'])) for name in members.keys()], key = lambda x: (-x[2])):
     user = members[member]
-    print(user['name'], ':', stars, '*', '\t\tscore:', local_score)
-    out_file.write(str(user['name']) + ':'+  str(stars) + '*' +  '\t\tscore: ' + str(+ local_score) + '\n')
+    out_file.write(str(place) + ") " + str(user['name']) + ': '+  str(stars) + '*' +  '\t\tscore: ' + str(+ local_score) + '\n')
     daily_info = members[member]['completion_day_level']
     for day in sorted(daily_info.keys(), key = lambda x: int(x)):
         part_one_time_string, part_two_time_string = '',''
@@ -17,8 +17,7 @@ for member, stars, local_score in sorted([(name, int(members[name]['stars']), in
             part_two_time_string = dateutil.parser.parse(daily_info[day]['2']['get_star_ts']).strftime('%I:%M:%S %p %m/%d/%y')
         if len(day) == 1:
             day = '0' + day
-        print('Day', day, ':  Part 1: ', part_one_time_string, '\tPart 2:', part_two_time_string)
         out_file.write('Day ' +  str(day) +  ':  Part 1: ' + part_one_time_string +  '\tPart 2: ' +  part_two_time_string+ '\n')
-    print('\n')
     out_file.write('\n')
+    place += 1
 out_file.close()
