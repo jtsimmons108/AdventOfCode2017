df6ad48e080dddc8c03db64e517387dbff8c1233
jtsimmons108 <jsimmons@psdr3.org>
2017-12-04 23:58:50 -0600

    finished day 5 python and kotlin

---
+++ kotlin/src/main/day5.kt
@@ -0,0 +1,27 @@
+package main
+
+import java.io.File
+
+fun main(args: Array<String>){
+
+    val input = File("inputs/day5.in").readLines()
+            .map { it.toInt() }
+
+    for(part in 1..2) {
+        val instructions = input.toMutableList()
+        var (i, steps) = Pair(0, 0)
+
+        while (i < instructions.size) {
+            val off = instructions[i]
+            when(part){
+                1 -> instructions[i]++
+                2 -> if(instructions[i] >= 3) instructions[i]-- else instructions[i]++
+            }
+            i += off
+            steps++
+        }
+        
+        println("Part $part: $steps")
+    }
+
+}
---
+++ python/Day5/day5.py
@@ -0,0 +1,21 @@
+import re, itertools, hashlib, time
+
+
+inpt = open('day5.in').read().strip()
+
+data = list(map(int, inpt.strip().splitlines()))
+total = 0
+i = 0
+start = time.time()
+while i < len(data):
+    next = i + data[i]
+    if data[i] >= 3:
+        data[i] -= 1
+    else:
+        data[i] += 1
+    i = next
+    total += 1
+
+
+print(time.time() - start)
+print(total)
