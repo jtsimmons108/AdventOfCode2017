c2944258eb78a780d1512d7e31c201dceceb1fc8
jtsimmons108 <jsimmons@psdr3.org>
2017-12-09 22:45:49 -0600

    added kotlin solution

--- kotlin/src/main/day8.kt
+++ kotlin/src/main/day8.kt
@@ -1,5 +1,46 @@
 package main
 
+import java.io.File
+
+val registers: MutableMap<String, Int> = HashMap<String, Int>().withDefault { _ -> 0 }
+
 fun main(args: Array<String>){
 
+
+
+    val input = File("inputs/day8.in").readLines()
+    var maxValue = Integer.MIN_VALUE
+    input.map {
+
+        val values = it.split(" ")
+        if(checkCondition(values[4], values[5], values[6])) {
+            var toAdd = values[2].toInt()
+            if (values[1] == "dec")
+                toAdd *= -1
+            registers[values[0]] = registers.getValue(values[0]) + toAdd
+        }
+
+        if(!registers.values.isEmpty())
+            maxValue = maxOf(maxValue, registers.values.max()!!)
+
+    }
+
+    println("Part 1: ${registers.values.max()}")
+    println("Part 2: $maxValue")
+
+}
+
+fun checkCondition(reg: String, op: String, value: String) : Boolean {
+    val left = registers.getValue(reg)
+    val right = value.toInt()
+    return when (op) {
+        "==" -> left == right
+        "!=" -> left != right
+        "<=" -> left <= right
+        ">=" -> left >= right
+        "<" -> left < right
+        ">" -> left > right
+        else -> false
+
+    }
 }
