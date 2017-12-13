7216ebbea0e9a1a526ccee9bcaf77352dbb47d55
jtsimmons108 <jsimmons@psdr3.org>
2017-12-13 07:24:39 -0600

    Solved day 13

---
+++ kotlin/src/main/day13.kt
@@ -0,0 +1,24 @@
+package main
+
+import java.io.File
+import kotlin.system.measureTimeMillis
+
+
+fun main(args: Array<String>) {
+
+    val input = File("inputs/day13.in").readLines()
+    val values = input.map { it.split(": ").map { it.toInt() } }
+            .associate { it.first() to it.last() }
+
+    val severity = values.entries
+            .map { if (it.key % (2 * (it.value - 1)) == 0) it.key * it.value else 0 }
+            .sum()
+
+    var delay = 0
+    while (values.entries.any { (it.key + delay) % (2 * (it.value - 1)) == 0 })
+        delay++
+
+    println("Part 1: $severity")
+    println("Part 2: $delay")
+
+}
---
+++ python/Day13/day13.py
@@ -0,0 +1,22 @@
+
+scanners = {}
+
+for line in open('day13.in'):
+    depth, cycle = map(int, line.split(": "))
+    scanners[depth] = cycle
+
+severity = sum(map(lambda s: s * scanners[s] if s % (2 * (scanners[s] - 1)) == 0 else 0, scanners.keys()))
+
+def makes_clean_pass(delay):
+    for d, r in scanners.items():
+        if (d + delay) % (2 * (r - 1)) == 0:
+            return False
+    return True
+
+
+delay = 0
+while not makes_clean_pass(delay):
+    delay += 1
+
+print('Part 1: {}'.format(severity))
+print('Part 2: {}'.format(delay))
